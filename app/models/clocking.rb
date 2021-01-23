fc093e99a66e772e2b53697d47d57d9c36c0edbb
bigwizzo <wnnwillard@gmail.com>
2021-01-24 00:37:06 +0200

    Add validation to Clocking model and use url helpers in features/subject_spec

--- app/models/clocking.rb
+++ app/models/clocking.rb
@@ -1,3 +1,7 @@
 class Clocking < ApplicationRecord
   belongs_to :subject
+
+  validates :topic, presence: true, length: { minimum: 10, maximum: 40 }, uniqueness: true
+  validates :details, presence: true, length: { minimum: 10, maximum: 40 }
+  validates :duration, presence: true
 end
--- spec/features/subject_spec.rb
+++ spec/features/subject_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'Subject' do
   before :each do
     Student.create(username: 'myname', email: 'myname@email.com', password: 'mypassword',
                 password_confirmation: 'mypassword')
-    visit 'students/sign_in'
+    visit student_session_path
     fill_in 'student[username]', with: 'myname'
     fill_in 'student[password]', with: 'mypassword'
     click_on 'commit'
   end
 
   it 'Create a subject for the signed in student' do
-    visit '/subjects/new'
+    visit new_subject_path
     fill_in 'subject[title]', with: 'Subject title'
     fill_in 'subject[description]', with: 'Subject description'
     click_on 'commit'
@@ -19,14 +19,14 @@ RSpec.describe 'Subject' do
   end
 
   it 'Fail to create subject if title is blank' do
-    visit '/subjects/new'
+    visit new_subject_path
     fill_in 'subject[title]', with: ''
     click_on 'commit'
     expect(page).to have_text("Title can't be blank")
   end
 
   it 'Fail to create subject if title is too short' do
-    visit '/subjects/new'
+    visit new_subject_path
     fill_in 'subject[title]', with: ''
     click_on 'commit'
     expect(page).to have_text("Title is too short")
