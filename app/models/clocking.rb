c8a9087dc437f298487247c35f8b39c7bfa4b7c5
bigwizzo <wnnwillard@gmail.com>
2021-01-24 04:20:45 +0200

    Attach images to subjects and clockings except for external

--- app/models/clocking.rb
+++ app/models/clocking.rb
@@ -1,6 +1,6 @@
 class Clocking < ApplicationRecord
   belongs_to :subject
-  belongs_to :student
+  # belongs_to :student
 
   validates :topic, presence: true, length: { minimum: 10, maximum: 40 }, uniqueness: true
   validates :details, presence: true, length: { minimum: 10, maximum: 40 }
