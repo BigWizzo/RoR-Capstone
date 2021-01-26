b6256da3a2e4f50dab47ff186c89e6b923fee620
bigwizzo <wnnwillard@gmail.com>
2021-01-26 18:49:40 +0200

    Remove logic for checking external subject from the subject index

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,2 +1,9 @@
 module SubjectsHelper
+  def check_external(subject)
+    if subject.title != "External"
+      image_tag(subject.icon.variant(extent: "10:7"), class: "card-img")
+    elsif subject.title == "External"
+      image_tag("default.jpg", class: "card-img")
+    end
+  end
 end
