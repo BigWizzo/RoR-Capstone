cecbb3e6e8c64c1660311e5af80a3b3dde0bd70a
bigwizzo <wnnwillard@gmail.com>
2021-01-26 11:38:58 +0200

    Count total number of subject based clockings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def count_clock
+    count = -1
+    @subjects.each do |subject|
+      subject.clockings.each do |clocking|
+        count = count + clocking.duration
+      end
+    end
+    count
+  end
 end
