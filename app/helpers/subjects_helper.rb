1e05f2667c9496c39ee73be7b9d186b01d8e6344
bigwizzo <wnnwillard@gmail.com>
2021-01-27 21:52:26 +0200

    Add image tag for active storage

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,9 +1,10 @@
 module SubjectsHelper
   def check_external(subject)
-    if subject.title != 'External' && subject.icon.attached?
+    if subject.title != 'External' && subject.icon.present?
+      image_tag(subject.icon, class: "card-img")
       # image_tag(url_for(subject.icon.variant(extent: '10:7')), class: 'card-img')
-      image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
-    elsif subject.title != 'External' && !subject.icon.attached?
+      # image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
+    elsif subject.title != 'External' && !subject.icon.present?
       image_tag('placeholder.jpg', class: 'card-img')
     elsif subject.title == 'External'
       image_tag('default.jpg', class: 'card-img')
