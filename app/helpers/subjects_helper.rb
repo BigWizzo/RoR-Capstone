2bf405a8e99a1d0222b63677a5e874ceb5ebeb3a
bigwizzo <wnnwillard@gmail.com>
2021-01-28 00:42:57 +0200

    Add crop to variant

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -3,7 +3,7 @@ module SubjectsHelper
     if subject.title != 'External' && subject.icon.attached?
       # image_tag(subject.icon, class: "card-img")
       # image_tag(url_for(subject.icon.variant(extent: '10:7')), class: 'card-img')
-      image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
+      image_tag subject.icon.variant(crop: '500x350+0+0'), class: 'card-img'
     elsif subject.title != 'External' && !subject.icon.attached?
       image_tag('placeholder.jpg', class: 'card-img')
     elsif subject.title == 'External'
