6517191f52139c42916607fb6f4bc368030a0afe
bigwizzo <wnnwillard@gmail.com>
2021-01-27 22:34:43 +0200

    Add resizing of the image

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,7 +1,7 @@
 module SubjectsHelper
   def check_external(subject)
     if subject.title != 'External' && subject.icon.present?
-      image_tag(subject.icon, class: "card-img")
+      image_tag(subject.icon.variant(resize: '500x350'), class: "card-img")
       # image_tag(url_for(subject.icon.variant(extent: '10:7')), class: 'card-img')
       # image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
     elsif subject.title != 'External' && !subject.icon.present?
