dbd41cb77ec5c172d00f0030ebc73a1c040c3c3e
bigwizzo <wnnwillard@gmail.com>
2021-01-27 22:10:56 +0200

    Change .attached? to .present?

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,9 +1,9 @@
 module SubjectsHelper
   def check_external(subject)
     if subject.title != 'External' && subject.icon.present?
-      image_tag(subject.icon, class: "card-img")
+      # image_tag(subject.icon, class: "card-img")
       # image_tag(url_for(subject.icon.variant(extent: '10:7')), class: 'card-img')
-      # image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
+      image_tag subject.icon.variant(extent: '10:7'), class: 'card-img'
     elsif subject.title != 'External' && !subject.icon.present?
       image_tag('placeholder.jpg', class: 'card-img')
     elsif subject.title == 'External'
