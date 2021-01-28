90b0a2e6824a30a5b676abfe91b6ce211d9a31da
bigwizzo <wnnwillard@gmail.com>
2021-01-28 06:26:33 +0200

    Add README file

--- app/models/clocking.rb
+++ app/models/clocking.rb
@@ -1,7 +1,7 @@
 class Clocking < ApplicationRecord
   belongs_to :subject
 
-  validates :topic, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
+  validates :topic, presence: true, length: { minimum: 3, maximum: 20 }
   validates :details, presence: true, length: { minimum: 6, maximum: 30 }
   validates :duration, presence: true
 end
