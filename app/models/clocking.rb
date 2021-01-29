02c29a082da84b04cb6d45bfae523f895212c159
bigwizzo <wnnwillard@gmail.com>
2021-01-29 22:24:36 +0200

    Add subject ID and remove external/internal columns

--- app/models/clocking.rb
+++ app/models/clocking.rb
@@ -1,6 +1,5 @@
 class Clocking < ApplicationRecord
-  belongs_to :internal, class_name: "Subject", optional: true
-  belongs_to :external, class_name: "Subject", optional: true
+  belongs_to :subject, optional: true
   belongs_to :student
 
   validates :topic, presence: true, length: { minimum: 3, maximum: 20 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_180426) do
+ActiveRecord::Schema.define(version: 2021_01_29_201707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,11 +43,9 @@ ActiveRecord::Schema.define(version: 2021_01_29_180426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "student_id"
-    t.bigint "internal_id"
-    t.bigint "external_id"
-    t.index ["external_id"], name: "index_clockings_on_external_id"
-    t.index ["internal_id"], name: "index_clockings_on_internal_id"
+    t.bigint "subject_id"
     t.index ["student_id"], name: "index_clockings_on_student_id"
+    t.index ["subject_id"], name: "index_clockings_on_subject_id"
   end
 
   create_table "students", force: :cascade do |t|
