5d5150d5dea73ea10d9f2ebf4c7b3c53156df92e
bigwizzo <wnnwillard@gmail.com>
2021-01-29 20:42:38 +0200

    make internal/external IDs asscoiations with subject optional

--- app/models/clocking.rb
+++ app/models/clocking.rb
@@ -1,6 +1,7 @@
 class Clocking < ApplicationRecord
-  belongs_to :internal, class_name: "Subject"
-  belongs_to :external, class_name: "Subject"
+  belongs_to :internal, class_name: "Subject", optional: true
+  belongs_to :external, class_name: "Subject", optional: true
+  belongs_to :student
 
   validates :topic, presence: true, length: { minimum: 3, maximum: 20 }
   validates :details, presence: true, length: { minimum: 6, maximum: 30 }
